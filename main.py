802c00ee4c391f8a865bf1cb08aceebe349b2a67
brnpal <brendan.allaway@gmail.com>
2024-09-27 02:28:49 -0700

    changes

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 def main():
     book_path = "books/frankenstein.txt"
+    retrieve_book(book_path)
     count_words(book_path)
     count_chars(book_path)
-    with open(book_path) as f:
-        return f.read()
-    
 
-def count_words(text):
-    words = text.split()
+def retrieve_book(book):
+    with open(book) as f:
+     return f.read()
+
+def count_words(book):
+    words = book.split()
     print(f"There were {len(words)} words in this book.")
 
 def count_chars(book):
     book_lower_case = book.lower()
-    chars = {"a", "b", "c"}
+    chars = {"a", "b", "c", }
     count_dict = {}
     for char in chars:
         count = 0
@@ -20,6 +22,8 @@ def count_chars(book):
             if char == book_char:
                 count += 1
         count_dict.update({char:count})
-    return count_dict
+        print(f"The {char} character was found {count} times.")
+    # WIP, sort chars highest to lowest
+
 
 main()
